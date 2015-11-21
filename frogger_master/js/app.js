96602200090d7a039f175c33b6e3b784dcc655e5
Cory Kane <ckane30691@gmail.com>
2015-11-21 15:18:32 -0800

    Instantiate objects

--- frogger_master/js/app.js
+++ frogger_master/js/app.js
@@ -38,7 +38,8 @@ Player.prototype.render = function(){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
