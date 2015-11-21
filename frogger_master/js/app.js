161e219fc8cc55245e434389f49d2469f825652d
Cory Kane <ckane30691@gmail.com>
2015-11-21 15:13:57 -0800

    Begin add player class.

--- frogger_master/js/app.js
+++ frogger_master/js/app.js
@@ -24,8 +24,17 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt){
 
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
