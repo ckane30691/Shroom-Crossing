b94cb528c7a1e7a441ec0f867501022e588da7c2
Cory Kane <ckane30691@gmail.com>
2018-03-20 19:35:02 -0700

    install dependency and import passport in app.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 // Set up the express app
 const app = express();
 
 // Log requests to the console.
 app.use(logger('dev'));
+
+// required for passport
+app.use(session({ secret: 'testSecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
